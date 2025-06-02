00d92d0b8f3824983e647f33b8ccc442fa6c4270
marufrobin <marufrobin5@gmail.com>
2025-06-02 19:54:53 +0600

    Refactor main.dart: Introduce RepositoryProvider and MultiBlocProvider for improved state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:minimal_expense_tracker/repository/expanse.repository.dart';
 import 'package:minimal_expense_tracker/screens/home/home.screen.dart';
 import 'package:minimal_expense_tracker/themes/app_theme.dart';
+import 'package:bloc/bloc.dart';
 
+import 'bloc/expanse_bloc/expanse_bloc.dart';
+import 'bloc/theme_change_bloc/theme_change_cubit.dart';
 import 'database/expanse_database.dart';
 
 Future<void> main() async {
@@ -18,11 +23,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minimal Expense Tracker',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: HomeScreens(),
+    return RepositoryProvider(
+      create: (context) => ExpenseRepository(ExpanseDatabase()),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => ThemeChangeCubit()),
+          BlocProvider(
+            create: (context) => ExpanseBloc(context.read<ExpenseRepository>()),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Minimal Expense Tracker',
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: HomeScreens(),
+        ),
+      ),
     );
   }
 }
