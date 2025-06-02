200ef71a39d0a0d55ee8fa978cc9acc837f2dce1
marufrobin <marufrobin5@gmail.com>
2025-06-02 14:51:32 +0600

    Implement ExpanseChartScreen with BarChart visualization and data model

--- lib/screens/expanse_chart/expanse_chart.screen.dart
+++ lib/screens/expanse_chart/expanse_chart.screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class ExpanseChartScreen extends StatelessWidget {
@@ -5,6 +6,42 @@ class ExpanseChartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final List<BarChartDataModel> data = [
+      BarChartDataModel(x: 0, y: 10),
+      BarChartDataModel(x: 1, y: 20),
+      BarChartDataModel(x: 2, y: 30),
+      BarChartDataModel(x: 3, y: 40),
+      BarChartDataModel(x: 4, y: 50),
+      BarChartDataModel(x: 5, y: 60),
+      BarChartDataModel(x: 6, y: 70),
+      BarChartDataModel(x: 7, y: 80),
+      BarChartDataModel(x: 8, y: 90),
+      BarChartDataModel(x: 9, y: 100),
+    ];
+    return SizedBox(
+      height: 300,
+      width: width,
+      child: BarChart(
+        BarChartData(
+          barGroups: List.generate(data.length, (index) {
+            final singleData = data[index];
+            return BarChartGroupData(
+              x: singleData.x ?? 0,
+              barRods: [BarChartRodData(toY: singleData.y ?? 0)],
+            );
+          }),
+        ),
+      ),
+    );
   }
+}
+
+class BarChartDataModel {
+  final int? x;
+  final double? y;
+
+  BarChartDataModel({this.x, this.y});
 }
