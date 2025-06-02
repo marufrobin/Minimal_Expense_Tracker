6ae3e2b8bce4ff4de14f4fb725f2a7e2d9c31b5c
marufrobin <marufrobin5@gmail.com>
2025-06-02 14:39:06 +0600

    Refactor HomeScreens to display ExpanseChartScreen and update pubspec.yaml to include fl_chart dependency

--- lib/screens/home/home.screen.dart
+++ lib/screens/home/home.screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_expense_tracker/screens/add_expense/add_expanse.screen.dart';
+import 'package:minimal_expense_tracker/screens/expanse_chart/expanse_chart.screen.dart';
 
 class HomeScreens extends StatelessWidget {
   const HomeScreens({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Minimal Expense Tracker')),
-      body: const Center(child: Text('Hello World!')),
+      body: Column(children: [ExpanseChartScreen()]),
       floatingActionButton: AddExpanseScreens(),
     );
   }
