b484b3b543bd6a3eb4eb6d83bc7f779b4de1ce3d
marufrobin <marufrobin5@gmail.com>
2025-06-02 20:29:30 +0600

    Refactor ExpanseListWidget: Integrate Bloc for state management and update item display logic

--- lib/screens/expanse_list/expanse_list.widget.dart
+++ lib/screens/expanse_list/expanse_list.widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+
+import '../../bloc/expanse_bloc/expanse_bloc.dart';
 
 class ExpanseListWidget extends StatelessWidget {
   const ExpanseListWidget({super.key});
@@ -8,33 +11,71 @@ class ExpanseListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final color = theme.colorScheme;
-    return ListView.builder(
-      itemCount: 10,
-      shrinkWrap: true,
-      itemBuilder: (context, index) {
-        return Slidable(
-          endActionPane: ActionPane(
-            motion: StretchMotion(),
-            children: [
-              SlidableAction(
-                onPressed: (context) {},
-                backgroundColor: color.onSurface.withValues(alpha: 0.1),
-                foregroundColor: color.onSurface,
-                borderRadius: BorderRadius.circular(8),
-                icon: Icons.delete,
-              ),
-              SlidableAction(
-                onPressed: (context) {},
-                backgroundColor: color.error,
-                foregroundColor: color.onSurface,
-                borderRadius: BorderRadius.circular(8),
-                icon: Icons.settings,
-              ),
-            ],
-          ),
-          child: ListTile(title: Text('Item $index'), trailing: Text("data")),
+    return BlocBuilder<ExpanseBloc, ExpanseState>(
+      builder: (context, state) {
+        return ListView.builder(
+          itemCount: state is ExpenseLoaded ? state.expenses.length : 1,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            if (state is ExpenseLoaded) {
+              return _body(
+                color: color,
+                id: state.expenses[index].id,
+                title: state.expenses[index].name,
+                trailing: state.expenses[index].amount?.toString(),
+              );
+            } else if (state is ExpenseLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is ExpenseError) {
+              return const Center(child: Text('Error'));
+            } else if (state is ExpenseOperationSuccess) {
+              return _body(
+                color: color,
+                id: state.expenses[index].id,
+                title: state.message,
+                trailing: state.expenses[index].amount?.toString(),
+              );
+            } else {
+              return const Center(child: Text('No data'));
+            }
+          },
         );
       },
+    );
+  }
+
+  Slidable _body({
+    required ColorScheme color,
+    required int id,
+    required String? title,
+    required String? trailing,
+  }) {
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: StretchMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              context.read<ExpanseBloc>().add(DeleteExpenseEvent(id));
+            },
+            backgroundColor: color.onSurface.withValues(alpha: 0.1),
+            foregroundColor: color.onSurface,
+            borderRadius: BorderRadius.circular(8),
+            icon: Icons.delete,
+          ),
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: color.error,
+            foregroundColor: color.onSurface,
+            borderRadius: BorderRadius.circular(8),
+            icon: Icons.settings,
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(title ?? "No data"),
+        trailing: Text(trailing ?? "No data"),
+      ),
     );
   }
 }
